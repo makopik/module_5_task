45ab755f64c604168f6d21eeb29e8d6ab5164385
makopik <mr.krasovskiy.m@mail.ru>
2021-04-04 22:08:43 +0300

    рп

--- Pr1.py
+++ Pr1.py
@@ -1,32 +1,43 @@
 from collections import deque
 
-def delen(x, y):
-    Nom = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
-               'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15,
-               0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
-               10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+
+def f(x, y):
+    OCT_NUM = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, 
+               0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7',}
     result = deque()
     transfer = 0
+
     if len(y) > len(x):
         x, y = deque(y), deque(x)
+
     else:
         x, y = deque(x), deque(y)
+
     while x:
+
         if y:
-            res = Nom[x.pop()] / Nom[y.pop()] + transfer
+            res = OCT_NUM[x.pop()] - OCT_NUM[y.pop()] - transfer
+
         else:
-            res = Nom[x.pop()] / transfer
+            res = OCT_NUM[x.pop()] - transfer
+
         transfer = 0
-        if res < 16:
-            result.appendleft(Nom[res])
+
+        if res < 8:
+            result.appendleft(OCT_NUM[res])
+
         else:
-            result.appendleft(Nom[res - 16])
+            result.appendleft(OCT_NUM[res - 8])
             transfer = 1
+
     if transfer:
         result.appendleft('1')
-    return list(result)
-a = list(input('Введите 1-е шестнадцатиричное число: ').upper())
-b = list(input('Введите 2-е шестнадцатиричное число: ').upper())
 
-print(*a, '/', *b, '=', *delen(a, b))
+    return list(result)
+
+a = list (input('Введите 1-е восьмиричное число: ').upper())
+b = list (input('Введите 2-е восьмиричное число: ').upper())
+print(a, b)
+
+print(*a, '-', *b, '=', *f(a, b))
 
